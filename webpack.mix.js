ae4d42e5b4b5bffe6c9ed2d1a81b845866db8b4a
Johan Maes <johan.maes1@gmail.com>
2020-12-10 11:39:38 +0100

    debug build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
-let mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
+let mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss");
 
-mix.setPublicPath('dist')
-    .js('resources/js/tool.js', 'js')
-    .sass('resources/sass/tool.scss', 'css')
-    .options({
-        processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
-    });
+mix
+  .setPublicPath("dist")
+  .js("resources/js/tool.js", "js")
+  .sass("resources/sass/tool.scss", "css")
+  .options({
+    processCssUrls: false,
+    postCss: [tailwindcss("./tailwind.config.js")],
+  });
