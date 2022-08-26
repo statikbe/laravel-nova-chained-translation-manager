f8e133eed17abbc008358e31b7a912a64625e0dc
Nelu Moraru <nelumoraru007@gmail.com>
2022-08-26 14:12:20 +0300

    Maybe

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,11 +2,15 @@
 let mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
 
+require('./nova.mix')
+
 mix
-  .setPublicPath("dist")
-  .js("resources/js/tool.js", "js").vue()
-  .sass("resources/sass/tool.scss", "css")
-  .options({
-    processCssUrls: false,
-    postCss: [tailwindcss("./tailwind.config.js")],
-  });
+.setPublicPath('dist')
+.js('resources/js/tool.js', 'js')
+.vue({ version: 3 })
+.sass('resources/sass/tool.scss', 'css')
+.options({
+  processCssUrls: false,
+  postCss: [tailwindcss('./tailwind.config.js')]
+})
+.nova('statikbe/nova-translation-manager');
