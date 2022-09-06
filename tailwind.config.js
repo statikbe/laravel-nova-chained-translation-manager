95d5d34e41c7b8ec91b07becb9fd0d97a60a5ff0
Nelu Moraru <nelumoraru007@gmail.com>
2022-09-06 11:11:05 +0300

    Test tailwind build dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // File exists to make vscode-tailwindcss work
 module.exports = {
     content: ["./resources/js/**/*.vue", "./resources/js/**/*.js"],
+    darkMode: "class",
     theme: {},
     variants: {},
     plugins: [],
